a583e47faa1eccd00ba96dc75b588d3e984ccf6c
DasFranck <dasfranck@gmx.com>
2021-03-01 21:17:16 +0100

    Improve track_reader function

--- main.py
+++ main.py
@@ -10,12 +10,11 @@ from classes.Track import Track
 
 def track_reader(tracks_folder: str) -> List[Track]:
     track_list = []
-
-    for _, _, file_name in os.walk(tracks_folder):
-        if os.path.splitext(file_name)[1] == ".skiz":
-            logging.info("Creating Track from %s" % file_name)
-            Track.from_skiz_file(file_name)
-
+    for root, _, files in os.walk(tracks_folder):
+        for file_name in files:
+            if os.path.splitext(file_name)[1] == ".skiz":
+                logging.info("Creating Track from %s" % file_name)
+                track_list.append(Track.from_skiz_file(os.path.join(root, file_name)))
     return track_list
 
 def main():
@@ -25,6 +24,8 @@ def main():
     args = parser.parse_args()
 
     track_list = track_reader(args.tracks_folder)
+    for track in track_list:
+        print(track.attributes)
 
 if __name__ == "__main__":
     main()
